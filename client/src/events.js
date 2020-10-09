80d40065e79a2c4e037eda52a01c8f46b56e465f
typemoe <moe@pytrade.info>
2020-10-09 22:32:19 +0800

    updated feed content

--- client/src/events.js
+++ client/src/events.js
@@ -1,32 +1,36 @@
 let events = [
-    {
-        id: 123,
-        title: 'Event 1',
-        description: 'This is the first event',
-        createdAt: '25th December',
-        date: '30th December'
-    },
-    {
-        id: 123,
-        title: 'Event 2',
-        description: 'This is the first event',
-        createdAt: '25th December',
-        date: '30th December'
-    },
-    {
-        id: 123,
-        title: 'Event 3',
-        description: 'This is the first event',
-        createdAt: '25th December',
-        date: '30th December'
-    },
-    {
-        id: 123,
-        title: 'Event 4',
-        description: 'This is the first event',
-        createdAt: '25th December',
-        date: '30th December'
-    }   
-]
+  {
+    id: 1,
+    title: "Discovering Treasure Through Stories",
+    description:
+      "Year 2020 is an eventful year. The Covid-19 pandemic let us see the positive (selflessness) and negative (self-centeredness) traits of human nature. Seeing the importance of cultivating the positive traits of our next generation, we should start them from young by sowing good seeds in their minds and hearts, and continue to irrigate and fertilise them to let them thrive. As the saying goes: it takes 10 years to cultivate a tree, 100 years to nurture a human being.",
+    createdAt: "25th December",
+    date: "Saturday, Auguest 1, 2020",
+  },
+  {
+    id: 2,
+    title: "Standards For Being A Good Student and Child",
+    description:
+      "Year 2020 is an eventful year. The Covid-19 pandemic let us see the positive (selflessness) and negative (self-centeredness) traits of human nature. Seeing the importance of cultivating the positive traits of our next generation, we should start them from young by sowing good seeds in their minds and hearts, and continue to irrigate and fertilise them to let them thrive. As the saying goes: it takes 10 years to cultivate a tree, 100 years to nurture a human being.",
+    createdAt: "25th December",
+    date: "Saturday, October 31, 2020",
+  },
+  {
+    id: 3,
+    title: "Happy Formula",
+    description:
+      "Year 2020 is an eventful year. The Covid-19 pandemic let us see the positive (selflessness) and negative (self-centeredness) traits of human nature. Seeing the importance of cultivating the positive traits of our next generation, we should start them from young by sowing good seeds in their minds and hearts, and continue to irrigate and fertilise them to let them thrive. As the saying goes: it takes 10 years to cultivate a tree, 100 years to nurture a human being.",
+    createdAt: "25th December",
+    date: "Saturday, October 31, 2020",
+  },
+  {
+    id: 4,
+    title: "I Am Remarkable workshop for YOUth",
+    description:
+      " Come and join us in this empowering 90-minute virtual workshop on #IamRemarkable (a Google initiative) with Ms Eileen Seah, The Art of Career Pte Ltd.",
+    createdAt: "25th December",
+    date: "Saturday, October 31, 2020",
+  },
+];
 
-export default events
+export default events;
