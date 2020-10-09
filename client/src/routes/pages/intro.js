605d076d33592247e12cb9de1617e07df080abaf
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-10-09 20:19:46 +0800

    Update intro.js

--- client/src/routes/pages/intro.js
+++ client/src/routes/pages/intro.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import CurrentUserContext from '../../context/current-user.context'
-
+import { Input,Spin } from 'antd'
 const { Search } = Input
 
 const IntroPage = () => {
+    const [loading, setLoading]= useState(true)
     const [currentUser, setCurrentUser] = useContext(CurrentUserContext)
     return (
         <>
@@ -53,58 +54,14 @@ const IntroPage = () => {
                         <td></td>
                       </tr>
                       <tr>
-                        <td>Upcoming</td>
-                        <td><table>
-                          <tr>  //First event
-                            <td></td> //Event image here
-                            <td>
-                              <table> //Event details
-                                <tr></tr>
-                                <tr></tr>
-                              </table>
-                              <table> //features
-                                <tr>
-                                  <td><input type="checkbox" onclick="return false;"/>Sincerity</td>
-                                  <td><input type="checkbox" onclick="return false;"/>Service</td>
-                                </tr>
-                                <tr>
-                                  <td><input type="checkbox" onclick="return false;"/>Gratitude</td>
-                                  <td><input type="checkbox" onclick="return false;"/>Kindness</td>
-                                </tr>
-                              </table>
-                            </td>
-                          </tr>
-                        </table></td>
-                        <td></td>
+                        <td><script>document.write(new Data().getFullYear())</script></td>
                       </tr>
                       <tr>
-                        <td>Upcoming</td>
-                        <td><table>
-                          <tr>  //First event
-                            <td></td> //Event image here
-                            <td>
-                              <table> //Event details
-                                <tr></tr>
-                                <tr></tr>
-                              </table>
-                              <table> //features
-                                <tr>
-                                  <td><input type="checkbox" onclick="return false;"/>Sincerity</td>
-                                  <td><input type="checkbox" onclick="return false;"/>Service</td>
-                                </tr>
-                                <tr>
-                                  <td><input type="checkbox" onclick="return false;"/>Gratitude</td>
-                                  <td><input type="checkbox" onclick="return false;"/>Kindness</td>
-                                </tr>
-                              </table>
-                            </td>
-                          </tr>
-                        </table></td>
-                        <td></td>
+                        <td><script>document.write(new Data().getFullYear()-1)</script></td>
                       </tr>
                       </tbody>
                       </table></>
-                ):(
+                )):(
                     <p>You need to <Link to="/signup">Sign Up</Link> first!</p>
                 )
             }
