c12d5305f527b91994fab4a47ef8fe4c9edfacca
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-10-09 20:48:17 +0800

    Update intro.js

--- client/src/routes/pages/intro.js
+++ client/src/routes/pages/intro.js
@@ -23,45 +23,83 @@ const IntroPage = () => {
             <p>Check out the latest timeline of the upcoming events, and see what we have done before!</p>
             {
                 currentUser.token?(
-                    loading?(
-                        <Spin/>
-                    ):(
                       <><table>
                       <tbody id ="timeline">
                       <tr>
                         <td>Upcoming</td>
                         <td><table>
-                          <tr>  //First event
-                            <td></td> //Event image here
+                          <tr>
+                            <td>Event Image</td>
                             <td>
-                              <table> //Event details
-                                <tr></tr>
-                                <tr></tr>
-                              </table>
-                              <table> //features
+                              <table>
+                                <tr>Detail1</tr>
+                                <tr>Detail2</tr>
+                              </table></td>
+                              <td><table>
                                 <tr>
-                                  <td><input type="checkbox" onclick="return false;"/>Sincerity</td>
-                                  <td><input type="checkbox" onclick="return false;"/>Service</td>
+                                  <td>Sincerity</td>
+                                  <td>Service</td>
                                 </tr>
                                 <tr>
-                                  <td><input type="checkbox" onclick="return false;"/>Gratitude</td>
-                                  <td><input type="checkbox" onclick="return false;"/>Kindness</td>
+                                  <td>Gratitude</td>
+                                  <td>Kindness</td>
                                 </tr>
                               </table>
                             </td>
                           </tr>
                         </table></td>
-                        <td></td>
                       </tr>
                       <tr>
-                        <td><script>document.write(new Data().getFullYear())</script></td>
+                        <td>2020</td>
+                        <td><table>
+                          <tr>
+                            <td>Event Image</td>
+                            <td>
+                              <table>
+                                <tr>Detail1</tr>
+                                <tr>Detail2</tr>
+                              </table></td><td>
+                              <table>
+                                <tr>
+                                  <td>Sincerity</td>
+                                  <td>Service</td>
+                                </tr>
+                                <tr>
+                                  <td>Gratitude</td>
+                                  <td>Kindness</td>
+                                </tr>
+                              </table>
+                            </td>
+                          </tr>
+                        </table></td>
                       </tr>
                       <tr>
-                        <td><script>document.write(new Data().getFullYear()-1)</script></td>
+                        <td>2019</td>
+                        <td><table>
+                          <tr>
+                            <td>Event Image</td>
+                            <td>
+                              <table>
+                                <tr>Detail1</tr>
+                                <tr>Detail2</tr>
+                              </table></td><td>
+                              <table>
+                                <tr>
+                                  <td>Sincerity</td>
+                                  <td>Service</td>
+                                </tr>
+                                <tr>
+                                  <td>Gratitude</td>
+                                  <td>Kindness</td>
+                                </tr>
+                              </table>
+                            </td>
+                          </tr>
+                        </table></td>
                       </tr>
                       </tbody>
                       </table></>
-                )):(
+                ):(
                     <p>You need to <Link to="/signup">Sign Up</Link> first!</p>
                 )
             }
